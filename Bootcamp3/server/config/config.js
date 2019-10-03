11566e81d06c071aebffeaf56b502bcfd7d2e005
Irelis <44098389+Irelis@users.noreply.github.com>
2019-10-02 22:00:00 -0400

    Delete config.js

--- Bootcamp3/server/config/config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  db: {
-    uri: 'mongodb+srv://Irelis:54RXZnuYO6vTn9mm@cen3031-x7pqc.mongodb.net/UFMap?retryWrites=true&w=majority'
-  }, 
-  openCage: {
-    key: 'af9e0973b5e34e2a9e6ee58652567863'
-  },
-  port: 8080
-};
